64028ef3e2881cf12b411091507d18d959f6d67d
Junwen Qin <jwqin@thoughtworks.com>
2016-08-14 15:19:04 +0800

    Add eslint to check code style of javascript; Refactor npm scripts; Currently all the unit tests were excluded in eslint.

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  "env": {
+    "browser": true,
+    "commonjs": true,
+    "es6": true
+  },
+  "extends": "airbnb",
+  "rules": {
+    "quotes": [
+      "error",
+      "single",
+      {
+        "allowTemplateLiterals": true
+      }
+    ]
+  }
+};
