24636e0c2826fdfa753efebf9cbb8b0553bf022c
Junwen Qin <jwqin@thoughtworks.com>
2016-08-29 22:11:40 +0800

    add handleAction to generate reducer without switch-case statement

--- src/shared/utils/actionUtils.js
+++ src/shared/utils/actionUtils.js
@@ -2,14 +2,52 @@ import _ from 'lodash';
 
 export function getActionTypes(prefix, types) {
   const hasPrefix = arguments.length === 2;
+  const target = hasPrefix ? types : prefix;
 
-  return _.reduce(_.keys(types), (acc, v, k) => ({
+  return _.reduce(_.keys(target), (acc, k) => ({
     ...acc,
-    [k]: hasPrefix ? `${prefix}/${k}` : k,
+    [k]: hasPrefix ? `@@${prefix}/${k}` : k,
   }), {});
 }
 
+class ActionHandler {
+  constructor() {
+    this.currentAction = undefined;
+    this.handlers = {};
+  }
+  when(actionType, handler) {
+    this.currentAction = actionType;
+    this.handlers[actionType] = handler;
+    return this;
+  }
+  done(handler) {
+    this.handlers[`${this.currentAction}_COMPLETED`] = handler;
+    return this;
+  }
+  failed(handler) {
+    this.handlers[`${this.currentAction}_FAILED`] = handler;
+    return this;
+  }
+  build(initValue = null) {
+    return (state = initValue, action) => {
+      // debugger
+      const handler = this.handlers[action.type];
+
+      if (typeof handler === 'function') {
+        return handler(state, action);
+      }
+
+      return state;
+    };
+  }
+}
+
+export function handleActions() {
+  return new ActionHandler();
+}
+
 export default {
+  handleActions,
   getActionTypes,
 };
 
