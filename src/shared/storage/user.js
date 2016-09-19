71437d270044bbc3b6363ff9670cb67f074eb9f8
Junwen Qin <jwqin@thoughtworks.com>
2016-09-19 22:40:10 +0800

    refactor user storage helper method to independent functions

--- src/shared/storage/user.js
+++ src/shared/storage/user.js
@@ -1,13 +1,30 @@
+import { isObject } from 'lodash';
+
+const USER_STORAGE_KEY = 'user';
+
+function setUser(user) {
+  if (!isObject(user)) {
+    throw new TypeError('user should be an object');
+  }
+  window.localStorage.setItem(USER_STORAGE_KEY, JSON.stringify(user));
+  return Promise.resolve(user);
+}
+
+function removeUser() {
+  window.localStorage.removeItem(USER_STORAGE_KEY);
+
+  return Promise.resolve(null);
+}
+
+function getUser() {
+  const user = JSON.parse(window.localStorage.getItem(USER_STORAGE_KEY));
+
+  return user ? Promise.resolve(user) : Promise.reject();
+}
 
 export default {
-  setUser(user) {
-    window.localStorage.setItem('user', JSON.stringify(user));
-    return Promise.resolve(user);
-  },
-  getUser() {
-    const user = JSON.parse(window.localStorage.getItem('user'));
-
-    return user ? Promise.resolve(user) : Promise.reject();
-  },
+  setUser,
+  removeUser,
+  getUser,
 };
 
