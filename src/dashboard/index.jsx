062ecd5d64ae3313f14d038f734ec0ed7ec8b5b0
Junwen Qin <jwqin@thoughtworks.com>
2017-02-06 14:24:35 +0800

    export pure component for easier unit test

--- src/dashboard/index.jsx
+++ src/dashboard/index.jsx
@@ -3,10 +3,13 @@ import { connect } from 'react-redux';
 import accountActions from '../account/actions';
 import Navigation from './components/NavigationBar';
 
-const Dashboard = props => (
+const DashboardComponent = props => (
   <div className="dashboard">
     <Navigation {...props} />
+    <div>Hello world</div>
   </div>
 );
 
-export default connect(state => state, { logout: accountActions.logout })(Dashboard);
+export { DashboardComponent };
+
+export default connect(state => state, { logout: accountActions.logout })(DashboardComponent);
