7c286d9f3b96fdb1a9913ab985f7e7c7a0cba30f
Junwen Qin <jwqin@thoughtworks.com>
2017-02-06 12:54:14 +0800

    Refactor auth to use fetchApi

--- src/shared/api/auth.js
+++ src/shared/api/auth.js
@@ -1,27 +1,9 @@
-import 'whatwg-fetch';
-
-const baseUrl = process.env.config.server.url;
-
-function fetch2(url, config) {
-  return fetch(url, config)
-    .then(response => Promise.all([response, response.json()]))
-    .then(([response, responseJson]) => {
-      const isSuccess = response.status >= 200 && response.status < 300;
-      if (!isSuccess) {
-        throw responseJson;
-      }
-      return responseJson;
-    });
-}
+import fetchApi from './fetchApi';
 
 export default {
   login(user) {
-    return fetch2(`${baseUrl}/auth/login`, {
+    return fetchApi('auth/login', {
       method: 'POST',
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-      },
       body: JSON.stringify(user),
     });
   },
